f39a28fcb7669e938fe72790475a5aca4e9ed940
Kishan9896 <ckishan9896@gmail.com>
2022-07-04 11:29:23 +0530

    patient-detail

--- src/container/Medicines/Medicines.js
+++ src/container/Medicines/Medicines.js
@@ -8,10 +8,22 @@ import DialogTitle from "@mui/material/DialogTitle";
 import { Form, Formik, useFormik } from "formik";
 import * as yup from "yup";
 import { DataGrid } from "@mui/x-data-grid";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
+import Slide from "@mui/material/Slide";
+import DialogContentText from "@mui/material/DialogContentText";
+import EditIcon from "@mui/icons-material/Edit";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 function Medicines(props) {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
   const [data, setData] = useState([]);
+  const [deleteAlert, setDeleteAlert] = useState(false);
+  const [rowData, setRowdata] = useState(null);
+  const [edit, setEdit] = useState(false);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -19,6 +31,7 @@ function Medicines(props) {
 
   const handleClose = () => {
     setOpen(false);
+    formikOrg.resetForm();
   };
 
   let schema = yup.object().shape({
@@ -30,14 +43,41 @@ function Medicines(props) {
 
   const inserthandle = (values) => {
     const localData = JSON.parse(localStorage.getItem("Medicines"));
+
+    const id = Math.floor(Math.random() * 1000);
+
+    const dataIn = {
+      id: id,
+      ...values,
+    };
+
     if (localData === null) {
-      localStorage.setItem("Medicines", JSON.stringify([values]));
+      localStorage.setItem("Medicines", JSON.stringify([dataIn]));
     } else {
-      localData.push(values);
+      localData.push(dataIn);
       localStorage.setItem("Medicines", JSON.stringify(localData));
     }
     handleClose();
+    local();
   };
+
+
+  const dataEdit = (values) => {
+    const dataEditupdate = JSON.parse(localStorage.getItem("Medicines"));
+
+    const uData = dataEditupdate.map((P) => {
+      if (P.id === values.id) {
+        return values;
+      } else {
+        return P;
+      }
+    })
+    localStorage.setItem("Medicines", JSON.stringify(uData));
+
+    handleClose()
+    local()
+    setEdit(false)
+  }
 
   const formikOrg = useFormik({
     initialValues: {
@@ -48,37 +88,83 @@ function Medicines(props) {
     },
     validationSchema: schema,
     onSubmit: (values, action) => {
-      inserthandle(values);
+      if (edit) {
+        dataEdit(values);
+      } else {
+        inserthandle(values);
+      }
       action.resetForm();
     },
   });
+
+  const Delete = () => {
+    const DeleteData = JSON.parse(localStorage.getItem("Medicines"));
+
+    const deleteHandel = DeleteData.filter((p) => p.id !== rowData.id);
+
+    setData(deleteHandel);
+
+    localStorage.setItem("Medicines", JSON.stringify(deleteHandel));
+    setDeleteAlert(false);
+  };
+
+  const handleDeleteAlertOpen = (params) => {
+    setDeleteAlert(true);
+    setRowdata(params);
+  };
+
+  const handleDeleteAlertClose = () => {
+    setDeleteAlert(false);
+  };
+
+  const handleEdit = (params) => {
+    setOpen(true);
+
+    setEdit(true);
+    formikOrg.setValues(params.row);
+  }
 
   const columns = [
     { field: "name", headerName: "Name", width: 130 },
     { field: "price", headerName: "Price", width: 130 },
     { field: "quntity", headerName: "Quntity", width: 130 },
     { field: "expiry", headerName: "Expiry", width: 130 },
+    {
+      field: "Action",
+      headerName: "Action",
+      width: 130,
+      renderCell: (params) => (
+        <>
+          <IconButton
+            aria-label="edit"
+            onClick={() => handleEdit(params)}
+          >
+            <EditIcon />
+          </IconButton>
+          <IconButton
+            aria-label="delete"
+            onClick={() => handleDeleteAlertOpen(params)}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </>
+      ),
+    },
   ];
 
   const local = () => {
     const Datahandle = JSON.parse(localStorage.getItem("Medicines"));
 
-    const id = Math.floor(Math.random()*1000);
-
-    const dataIn = {
-      id: id,
-      ...values
+    if (Datahandle !== null) {
+      setData(Datahandle);
     }
+  };
 
-    setData(Datahandle);
-  }
-
-  useEffect (() => {
+  useEffect(() => {
     local();
-  }, [])
-  
+  }, []);
 
-  const { handleSubmit, handleChange, handleBlur, errors, touched } = formikOrg;
+  const { handleSubmit, handleChange, handleBlur, errors, touched, values } = formikOrg;
 
   return (
     <div>
@@ -97,12 +183,13 @@ function Medicines(props) {
           />
         </div>
 
-        <Dialog open={open} onClose={handleClose}>
+        <Dialog fullWidth open={open} onClose={handleClose}>
           <DialogTitle>Add Medicine</DialogTitle>
           <Formik values={formikOrg}>
             <Form onSubmit={handleSubmit}>
               <DialogContent>
                 <TextField
+                  value={values.name}
                   margin="dense"
                   name="name"
                   label="Medicine Name"
@@ -114,6 +201,7 @@ function Medicines(props) {
                 />
                 <p>{errors.name && touched.name ? errors.name : ""}</p>
                 <TextField
+                  value={values.price}
                   margin="dense"
                   name="price"
                   label="Medicine Price"
@@ -125,6 +213,7 @@ function Medicines(props) {
                 />
                 <p>{errors.price && touched.price ? errors.price : ""}</p>
                 <TextField
+                  value={values.quntity}
                   margin="dense"
                   name="quntity"
                   label="Medicine Quntity"
@@ -136,6 +225,7 @@ function Medicines(props) {
                 />
                 <p>{errors.quntity && touched.quntity ? errors.quntity : ""}</p>
                 <TextField
+                  value={values.expiry}
                   margin="dense"
                   name="expiry"
                   label="Medicine Expiry"
@@ -149,12 +239,34 @@ function Medicines(props) {
               </DialogContent>
               <DialogActions>
                 <Button onClick={handleClose}>Cancel</Button>
-                <Button type="submit">submit</Button>
+                {
+                  edit ?
+                  <Button type="submit">Update</Button>
+                  :
+                  <Button type="submit">submit</Button>
+                }
               </DialogActions>
             </Form>
           </Formik>
         </Dialog>
       </div>
+      <Dialog
+        open={deleteAlert}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleDeleteAlertClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            Are you want to sure Delete?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDeleteAlertClose}>No</Button>
+          <Button onClick={Delete}>Yes</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
--- src/container/Patient/Patient.js
+++ src/container/Patient/Patient.js
@@ -1,9 +1,272 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import { Form, Formik, useFormik } from "formik";
+import * as yup from "yup";
+import { DataGrid } from "@mui/x-data-grid";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
+import Slide from "@mui/material/Slide";
+import DialogContentText from "@mui/material/DialogContentText";
+import EditIcon from "@mui/icons-material/Edit";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
 
 function Patient(props) {
+    const [popen, setPopen] = useState(false);
+    const [data, setData] = useState([]);
+    const [deleteAlert, setDeleteAlert] = useState(false);
+    const [rowData, setRowdata] = useState(null);
+    const [edit, setEdit] = useState(false);
+
+    const handleClickOpen = () => {
+        setPopen(true);
+    };
+
+    const handleClose = () => {
+        setPopen(false);
+        formikOrg.resetForm();
+    };
+
+    let schema = yup.object().shape({
+        name: yup.string().required("Please Enter Patient Name."),
+        number: yup.number().required("Please Enter Patient Number."),
+        condition: yup.string().required("Please Enter Patient Condition."),
+        insurance: yup.string().required("Please Enter Patient Insurance Detail."),
+    });
+
+    const inserthandle = (values) => {
+        const localData = JSON.parse(localStorage.getItem("Patient"));
+
+        const id = Math.floor(Math.random() * 1000);
+
+        const dataIn = {
+            id: id,
+            ...values,
+        };
+
+        if (localData === null) {
+            localStorage.setItem("Patient", JSON.stringify([dataIn]));
+        } else {
+            localData.push(dataIn);
+            localStorage.setItem("Patient", JSON.stringify(localData));
+        }
+        handleClose();
+        local();
+    };
+
+
+    const dataEdit = (values) => {
+        const dataEditupdate = JSON.parse(localStorage.getItem("Patient"));
+
+        const uData = dataEditupdate.map((P) => {
+            if (P.id === values.id) {
+                return values;
+            } else {
+                return P;
+            }
+        })
+        localStorage.setItem("Patient", JSON.stringify(uData));
+
+        handleClose()
+        local()
+        setEdit(false)
+    }
+
+    const formikOrg = useFormik({
+        initialValues: {
+            name: "",
+            number: "",
+            condition: "",
+            insurance: "",
+        },
+        validationSchema: schema,
+        onSubmit: (values, action) => {
+            if (edit) {
+                dataEdit(values);
+            } else {
+                inserthandle(values);
+            }
+            action.resetForm();
+        },
+    });
+
+    const Delete = () => {
+        const DeleteData = JSON.parse(localStorage.getItem("Patient"));
+
+        const deleteHandel = DeleteData.filter((p) => p.id !== rowData.id);
+
+        setData(deleteHandel);
+
+        localStorage.setItem("Patient", JSON.stringify(deleteHandel));
+        setDeleteAlert(false);
+    };
+
+    const handleDeleteAlertOpen = (params) => {
+        setDeleteAlert(true);
+        setRowdata(params);
+    };
+
+    const handleDeleteAlertClose = () => {
+        setDeleteAlert(false);
+    };
+
+    const handleEdit = (params) => {
+        setPopen(true);
+
+        setEdit(true);
+        formikOrg.setValues(params.row);
+    }
+
+    const columns = [
+        { field: "name", headerName: "Name", width: 130 },
+        { field: "number", headerName: "Number", width: 130 },
+        { field: "condition", headerName: "Condition", width: 130 },
+        { field: "insurance", headerName: "Insurance", width: 130 },
+        {
+            field: "Action",
+            headerName: "Action",
+            width: 130,
+            renderCell: (params) => (
+                <>
+                    <IconButton
+                        aria-label="edit"
+                        onClick={() => handleEdit(params)}
+                    >
+                        <EditIcon />
+                    </IconButton>
+                    <IconButton
+                        aria-label="delete"
+                        onClick={() => handleDeleteAlertOpen(params)}
+                    >
+                        <DeleteIcon />
+                    </IconButton>
+                </>
+            ),
+        },
+    ];
+
+    const local = () => {
+        const Datahandle = JSON.parse(localStorage.getItem("Patient"));
+
+        if (Datahandle !== null) {
+            setData(Datahandle);
+        }
+    };
+
+    useEffect(() => {
+        local();
+    }, []);
+
+    const { handleSubmit, handleChange, handleBlur, errors, touched, values } = formikOrg;
+
     return (
         <div>
-            <h2>Patient</h2>
+            <h1>Patient Detail</h1>
+            <div>
+                <Button variant="outlined" onClick={handleClickOpen}>
+                    Patient
+                </Button>
+                <div style={{ height: 400, width: "100%" }}>
+                    <DataGrid
+                        rows={data}
+                        columns={columns}
+                        pageSize={5}
+                        rowsPerPageOptions={[5]}
+                        checkboxSelection
+                    />
+                </div>
+
+                <Dialog fullWidth open={popen} onClose={handleClose}>
+                    <DialogTitle>Add Patient Details</DialogTitle>
+                    <Formik values={formikOrg}>
+                        <Form onSubmit={handleSubmit}>
+                            <DialogContent>
+                                <TextField
+                                    value={values.name}
+                                    margin="dense"
+                                    name="name"
+                                    label="Patient Name"
+                                    type="text"
+                                    fullWidth
+                                    variant="standard"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                />
+                                <p>{errors.name && touched.name ? errors.name : ""}</p>
+                                <TextField
+                                    value={values.number}
+                                    margin="dense"
+                                    name="number"
+                                    label="Patient Number"
+                                    type="number"
+                                    fullWidth
+                                    variant="standard"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                />
+                                <p>{errors.number && touched.number ? errors.number : ""}</p>
+                                <TextField
+                                    value={values.condition}
+                                    margin="dense"
+                                    name="condition"
+                                    label="Patient Condition"
+                                    type="text"
+                                    fullWidth
+                                    variant="standard"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                />
+                                <p>{errors.condition && touched.condition ? errors.condition : ""}</p>
+                                <TextField
+                                    value={values.insurance}
+                                    margin="dense"
+                                    name="insurance"
+                                    label="Patient Insurance"
+                                    type="text"
+                                    fullWidth
+                                    variant="standard"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                />
+                                <p>{errors.insurance && touched.insurance ? errors.insurance : ""}</p>
+                            </DialogContent>
+                            <DialogActions>
+                                <Button onClick={handleClose}>Cancel</Button>
+                                {
+                                    edit ?
+                                        <Button type="submit">Update</Button>
+                                        :
+                                        <Button type="submit">submit</Button>
+                                }
+                            </DialogActions>
+                        </Form>
+                    </Formik>
+                </Dialog>
+            </div>
+            <Dialog
+                open={deleteAlert}
+                TransitionComponent={Transition}
+                keepMounted
+                onClose={handleDeleteAlertClose}
+                aria-describedby="alert-dialog-slide-description"
+            >
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-slide-description">
+                        Are you want to sure Delete?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleDeleteAlertClose}>No</Button>
+                    <Button onClick={Delete}>Yes</Button>
+                </DialogActions>
+            </Dialog>
         </div>
     );
 }

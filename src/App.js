0df1beb6d9a633849df989471647a9e11c52683e
Kishan9896 <ckishan9896@gmail.com>
2022-08-04 15:17:15 +0530

    persist whitelist blacklist

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import Patient from "./container/Patient/Patient";
 import { Provider } from "react-redux";
 import Counter from "./container/Counter";
 import configureStore from "./container/redux/reducer/store";
+import { PersistGate } from 'redux-persist/integration/react'
 
 function App() {
-  const store = configureStore()
+  const { store, persistor } = configureStore();
 
   return (
     <Provider store={store}>
-      <Layout>
-        <Switch>
-          <Route path="/medicines" exact component={Medicines} />
-          <Route path="/patient" exact component={Patient} />
-          <Route path="/counter" exact component={Counter} />
-        </Switch>
-      </Layout>
+      <PersistGate loading={null} persistor={persistor}>
+        <Layout>
+          <Switch>
+            <Route path="/medicines" exact component={Medicines} />
+            <Route path="/patient" exact component={Patient} />
+            <Route path="/counter" exact component={Counter} />
+          </Switch>
+        </Layout>
+      </PersistGate>
     </Provider>
   );
 }
